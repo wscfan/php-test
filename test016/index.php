eedde62526279f38c56047c6f46aefdc8df58775
wangsong <wshappyday@sina.com>
2019-03-17 20:05:50 +0800

    2019.3.17

--- test016/index.php
+++ test016/index.php
@@ -1,22 +1,4 @@
 <?php
-<<<<<<< HEAD
-// header("content-type: text/html; charset=utf-8");
-
-$con = @mysql_connect('localhost', 'root', '');
-
-if (!$con) {
-  die('数据库连接失败' . mysql_error());
-} else {
-  mysql_query("SET NAMES UTF8");
-  mysql_select_db('test', $con);
-  $result = mysql_query("SELECT * FROM user");
-  while ($row = mysql_fetch_array($result)) {
-    var_dump($row);
-  }
-}
-
-mysql_close($con);
-=======
 
 $fk = fsockopen('localhost', 80, $errno, $errstr, 5);
 $s = array(
@@ -33,4 +15,3 @@ while ($row = fread($fk, 32)) {
 }
 
 fclose($fk);
->>>>>>> ee7d8dd30eb9b7d1310db140e955e0519ead2703
